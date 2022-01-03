a7ad27c6b3acf8c8c702d82873525ac32d7f4e66
JetzeJanssens <r0784103@student.thomasmore.be>
2022-01-03 15:53:36 +0100

    connection to db

--- gemeente-service/src/main/java/fact/it/gemeente/repository/GemeenteRepository.java
+++ gemeente-service/src/main/java/fact/it/gemeente/repository/GemeenteRepository.java
@@ -1,9 +1,12 @@
 package fact.it.gemeente.repository;
-
+import fact.it.gemeente.model.Gemeente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface GemeenteRepository {
+public interface GemeenteRepository extends JpaRepository<Gemeente, String> {
+    Gemeente findGemeenteByPostcode(String postcode);
+    List<Gemeente> findGemeenteByNaamContaining(String naam);
 
 }
