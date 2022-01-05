1376b2317f6f889e62a73a12fd947e94b346f062
JetzeJanssens <r0784103@student.thomasmore.be>
2022-01-05 14:51:19 +0100

    microservice met mongodb

---
+++ src/main/java/fact/it/gemeenteservice/controller/GemeenteController.java
@@ -0,0 +1,37 @@
+package fact.it.gemeenteservice.controller;
+
+import fact.it.gemeenteservice.model.Gemeente;
+import fact.it.gemeenteservice.repository.GemeenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@RestController
+public class GemeenteController {
+
+    @Autowired
+    private GemeenteRepository gemeenteRepository;
+
+    @PostConstruct
+    public void fillDB(){
+        if (gemeenteRepository.count()==0){
+            gemeenteRepository.save(new Gemeente("Ham", "3945"));
+            gemeenteRepository.save(new Gemeente("Tessenderlo","3980" ));
+        }
+        System.out.println("Gemeentes test:" + gemeenteRepository.findGemeenteByPostcode("3980").getNaam());
+    }
+
+    @GetMapping("/gemeentes/naam/{naam}")
+    public List<Gemeente> getGemeenteByName(@PathVariable String naam){
+        return gemeenteRepository.findGemeenteByNaam(naam);
+    }
+
+    @GetMapping("/gemeentes/{postcode}")
+    public Gemeente getGemeenteByPostcode(@PathVariable String postcode){
+        return gemeenteRepository.findGemeenteByPostcode(postcode);
+    }
+}
---
+++ src/test/java/fact/it/gemeenteservice/GemeenteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.gemeenteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemeenteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
